0a4ffdefa9a4473dece224dca2c4f105853248b3
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-02-24 23:09:29 +0900

    Title: 문자에 따른 명령 2, Time: 116ms, Memory: 26MB, Status: Passed - CodeTree

--- 240224/문자에 따른 명령 2/text-based-commands2.py
+++ 240224/문자에 따른 명령 2/text-based-commands2.py
@@ -4,12 +4,15 @@ dy = [0, -1, 0, 1]
 x = y = 0
 num = 3
 ls = list(input())
-for i in range(len(ls)):
-    if ls[i] == 'R':
-        num = (num+1)%4
-        x, y = dx[num], dy[num]
-    elif ls[i] == 'L':
-        num = (num+3)%4
-        x, y = dx[num], dy[num]
-    elif ls[i] == 'F':
-        print(x, y)
+if 'F' not in ls:
+    print(0, 0)
+else:
+    for i in range(len(ls)):
+        if ls[i] == 'R':
+            num = (num+1)%4
+        elif ls[i] == 'L':
+            num = (num+3)%4
+        elif ls[i] == 'F':
+            x += dx[num]
+            y += dy[num]
+    print(x, y)
