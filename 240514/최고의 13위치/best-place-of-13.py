4f4f0cdcb2d9cdf6a445df51865c76036a3173be
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-05-14 16:38:46 +0900

    Title: 최고의 13위치, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240514/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,11 @@
+n = int(input())
+ls = []
+for _ in range(n):
+    ls.append(list(map(int, input().split())))
+
+ans = 0
+for i in range(n):
+    for j in range(i, n-2):
+        num = ls[i][j] + ls[i][j+1] + ls[i][j+2]
+        ans = max(ans, num)
+print(ans)
