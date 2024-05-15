de32ef5f260a404a8ec973a59fd90765501b940a
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-05-15 20:18:08 +0900

    Title: 최고의 33위치, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240515/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,9 @@
+N = int(input())
+ls = [list(map(int, input().split())) for _ in range(N)]
+ans = 0
+for i in range(N-2):
+    num = 0
+    for j in range(N-2):
+        num += ls[i][j] + ls[i][j+1] + ls[i][j+2] + ls[i+1][j] + ls[i+1][j+1] + ls[i+1][j+2] + ls[i+2][j] + ls[i+2][j+1] + ls[i+2][j+2]
+        ans = max(ans, num)
+print(ans)
