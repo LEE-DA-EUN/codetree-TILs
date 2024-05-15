aabf5cd814d700bb3b36d52dbaa5f06a91ead811
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-05-15 23:10:34 +0900

    Title: 신기한 타일 뒤집기, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240515/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240515/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -2,19 +2,22 @@ n = int(input())
 num = 0
 min_num = 0
 max_num = 0
-dist = []
+before = ''
+
 for i in range(n):
     x, d = map(str, input().split())
     if d == 'R':
+        if before == 'R':
+            num -= 1
         num += int(x)
     elif d == 'L':
+        if before == 'L':
+            num += 1
         num -= int(x)
     min_num = min(num, min_num)
     max_num = max(num, max_num)
-    dist.append(d)
+    before = d
 
-if dist[0] == 'L':
-    num += 1
 white = abs(max_num - num)
 black = abs(num - min_num)
 
