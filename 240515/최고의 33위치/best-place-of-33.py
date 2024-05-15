f6c0df4da49170d0522ed561ac75b0564335ebc2
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-05-15 20:19:56 +0900

    Title: 최고의 33위치, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240515/최고의 33위치/best-place-of-33.py
+++ 240515/최고의 33위치/best-place-of-33.py
@@ -4,6 +4,6 @@ ans = 0
 for i in range(N-2):
     num = 0
     for j in range(N-2):
-        num += ls[i][j] + ls[i][j+1] + ls[i][j+2] + ls[i+1][j] + ls[i+1][j+1] + ls[i+1][j+2] + ls[i+2][j] + ls[i+2][j+1] + ls[i+2][j+2]
+        num = ls[i][j] + ls[i][j+1] + ls[i][j+2] + ls[i+1][j] + ls[i+1][j+1] + ls[i+1][j+2] + ls[i+2][j] + ls[i+2][j+1] + ls[i+2][j+2]
         ans = max(ans, num)
 print(ans)
